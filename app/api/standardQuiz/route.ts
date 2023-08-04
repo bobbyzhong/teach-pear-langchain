732176ade845baa87a65762ec5d814824c26a538
bobbyzhong <3bobbyzhong3@gmail.com>
2023-08-04 00:36:27 -0700

    removed bug in standardquiz api rout

--- app/api/standardQuiz/route.ts
+++ app/api/standardQuiz/route.ts
@@ -85,18 +85,3 @@ export async function POST(req: Request) {
     console.log(res);
     return NextResponse.json({ res });
 }
-
-Given the content generate a ${numQuestions} question quiz of ${difficulty} difficulty level. Give each question three answer choices (A,B,C) and return a 
-single JSON object. \n You must format your output as a JSON value that adheres to a given "JSON Schema" instance. "JSON Schema" is a declarative language that allows you to annotate 
-and validate JSON documents. For example, the example 
-"JSON Schema" instance {{"properties": {{"foo": {{"description": "a list of test words", "type": "array", "items": {{"type": "string"}}}}}}, "required": ["foo"]}}}} 
-would match an object with one required property, "foo". The "type" property specifies "foo" must 
-be an "array", and the "description" property semantically describes it as "a list of test words". The items within "foo" must be strings. 
-Thus, the object {{"foo": ["bar", "baz"]}} is a well-formatted instance of this example "JSON Schema". The object {{"properties": {{"foo": ["bar", "baz"]}}}} is not well-formatted. 
-Your output will be parsed and type-checked according to
- the provided schema instance, so make sure all fields in your output match the schema
-  exactly and there are no trailing commas! Here is the JSON Schema instance your output must adhere to.
-  {"type":"object","properties":{"questions":{"type":"string","description":"list containing every question. 
-  Ex: [Question 1, Question 2, etc.]"},"choices":{"type":"string","description":"list containing a list for each question's 3 possible answer choices labeled A, B, or C"},
-  "answers":{"type":"string","description":"list containing letters to each question's correct letter answer"}},"required":["questions","choices","answers"],
-  "additionalProperties":false,"$schema":"http://json-schema.org/draft-07/schema#"}\n${content}`;
